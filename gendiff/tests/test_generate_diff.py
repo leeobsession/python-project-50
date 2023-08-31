eab34037f66365e7bdacefc9747d62585280cd1a
leeobsession <lera03skor@gmail.com>
2023-08-31 21:02:32 +1000

    update test

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -3,22 +3,21 @@ from gendiff.gendiff import generate_diff
 
 def test_generate_diff():
 
-    data1 = 'gendiff/tests/fixtures/file1.json'
-    data2 = 'gendiff/tests/fixtures/file2.json'
-    data3 = 'gendiff/tests/fixtures/file_json1.json'
-    data4 = 'gendiff/tests/fixtures/file_json2.json'
+    data1 = 'gendiff/tests/fixtures/file_json1.json'
+    data2 = 'gendiff/tests/fixtures/file_json2.json'
 
-    result_json1 = \
+    result_json = \
         open('gendiff/tests/fixtures/result_json.txt').read()
-    result_json2 = \
-        open('gendiff/tests/fixtures/result_json1.txt').read()
+    result_stylish = \
+        open('gendiff/tests/fixtures/result_stylish.txt').read()
     result_plain = \
         open('gendiff/tests/fixtures/result_plain.txt').read()
 
-    diff_json1 = gener_diff(data1, data2)
-    diff_json2 = gener_diff(data3, data4)
-    diff_plain = gener_diff(data1, data2, 'plain')
+    diff_json1 = generate_diff(data1, data2)
+    diff_json2 = generate_diff(data1, data1)
+    diff_plain = generate_diff(data1, data2, 'plain')
 
-    assert diff_json1 == result_json1
-    assert diff_json2 == result_json2
-    assert result_plain == diff_plain
+    assert generate_diff(data1, data2, 'stylish') == result_stylish
+    assert generate_diff(data1, data2) == result_stylish
+    assert generate_diff(data1, data2, 'plain') == result_plain
+    assert generate_diff(data1, data2, 'json') == result_json
