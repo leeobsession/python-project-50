232b582656e9de4e70a95f34af609a6736d2eb52
leeobsession <lera03skor@gmail.com>
2023-08-31 21:07:17 +1000

    update scripts

--- gendiff/scripts/diff.py
+++ gendiff/scripts/diff.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from gendiff.gendiff import gener_diff
+from gendiff.gendiff import generate_diff
 from gendiff.parser import parse_args
 
 
 def main():
     args = parse_args()
-    print(gener_diff(args.first_file, args.second_file, args.format))
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == '__main__':
